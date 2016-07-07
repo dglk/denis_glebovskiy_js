e2a43d50a9b3633cbbf34bf994f0eaa3d59896d0
dglk <dglebovsky@gmail.com>
2016-07-07 14:54:48 +0300

    [03 Functions] 11 Functions as methods Using functions to implement object methods

---
+++ 03_functions/11_function_as_method/script.js
@@ -0,0 +1,10 @@
+(function() {
+  var Employee = {
+    'name': 'Ann',
+    'work': function() {
+      console.log('I am ' + this.name +'. I am working...');
+    }
+  };
+
+  Employee.work();
+})();
